aaf38577b197d474436c1b79fe9733d1fcd2ec72
G484472024 <248447@st.takushoku-u.ac.jp>
2025-05-22 17:23:47 +0900

    第五回練習問題5-7を作成

--- practice-dom2/dom2-exercise.js
+++ practice-dom2/dom2-exercise.js
@@ -15,4 +15,23 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+let b = document.querySelector('button#show');
+b.addEventListener('click', show);
 
+function show(){
+	let h = document.querySelector('h2#addr');
+	let p = document.createElement('p');
+	p.textContent = campus.address;
+	h.insertAdjacentElement('afterend',p);
+
+	
+	let u = document.createElement('ul');
+	h = document.querySelector('h2#dept'); 
+	h.insertAdjacentElement('afterend',u);
+	for(let department of gakka){
+		let l = document.createElement('li');
+		u.insertAdjacentElement('beforeend',l); 
+		l.textContent = department.name;
+		
+	}
+}
